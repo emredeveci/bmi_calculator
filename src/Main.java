87714e49ca77e5afda753b215991ad56f085c86c
Emre Deveci <23077383+emredeveci@users.noreply.github.com>
2024-02-20 16:22:31 +0300

    added notes

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //variable definitions
         double weight, height;
 
+        //scanner initialization
         Scanner input = new Scanner(System.in);
+
+        //ask for user inputs
         System.out.print("Please enter your height in meters: ");
         height = input.nextDouble();
 
         System.out.print("Please enter your weight in kilograms: ");
         weight = input.nextDouble();
 
+        //print the end result
         System.out.println("Your Body Mass Index is: " + weight / (height * height));
 
         input.close();
